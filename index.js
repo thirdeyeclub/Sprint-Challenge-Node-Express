f6fc35bf42f83159bf0b61b1cdd0889e1ea29d20
thirdeyeclub <wwwaaavvveeerrr@gmail.com>
2019-03-22 11:41:17 -0400

    wrote index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 // play this: https://www.youtube.com/watch?v=d-diB65scQU
+const server = require('./server');
+
+const PORT = process.env.PORT || 4000;
+
+server.listen(PORT, ()=>
+    console.log(`***Up and running on ${PORT}***`))
 
 // code away!
